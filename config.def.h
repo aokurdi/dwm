84c670f1178532c8008a0accae56a2d18333b1cb
aokurdi <a.o.kurdi2@gmail.com>
2021-04-26 11:35:25 +0900

    added bar height, barpadding, and created a build script

--- config.def.h
+++ config.def.h
@@ -2,14 +2,17 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 10;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monityor, >0: pin systray monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int ypad               = 10;       /* Pad above or below bar depending on bar position */
+static const int xpad               = 10;       /* Pad side padding around bar */
+static const int barheight          = 6;        /* Change bar height */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "VictorMono Nerd Font:size=13" };
 static const char dmenufont[]       = "VictorMono Nerd Font Mono:size=12";
